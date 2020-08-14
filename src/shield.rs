7d97f4f667a8a1a73f3fc53abee2eb6a8dd217aa
Acrimon <joel.wejdenstal@gmail.com>
2020-08-14 16:54:54 +0200

    feat: implement clone for Shield where possible

--- src/shield.rs
+++ src/shield.rs
@@ -11,6 +11,18 @@ impl<'a, R: Reclaimer> Shield<'a, R> {
     }
 }
 
+impl<'a, R: Reclaimer> Clone for Shield<'a, R>
+where
+    R::ShieldState: Clone
+    {
+        fn clone(&self) -> Self {
+            Self {
+                reclaimer: self.reclaimer,
+                state: self.state.clone(),
+            }
+        }
+    }
+
 impl<'a, R: Reclaimer> Drop for Shield<'a, R> {
     fn drop(&mut self) {
         self.reclaimer.destroy_shield(&mut self.state);

6b9a5aaadcb55d226697bcc466bebd3fc35ae0b5
Acrimon <joel.wejdenstal@gmail.com>
2020-08-16 16:04:54 +0200

    fix: only apply AtomicEbrExt to Ebr parameterized Atomics

--- src/ebr/atomic_ebr_ext.rs
+++ src/ebr/atomic_ebr_ext.rs
@@ -1,15 +1,16 @@
-use crate::{Atomic, Reclaimer, Shared, Shield, Tag};
+use crate::{Atomic, ReclaimableManager, Shared, Shield, Tag};
 use std::sync::atomic::Ordering;
+use super::Ebr;
 
 pub trait AtomicEbrExt {
-    type R: Reclaimer;
+    type M: ReclaimableManager;
     type V;
     type T: Tag;
 
     fn load<'shield>(
         &self,
         order: Ordering,
-        _shield: &'shield Shield<'_, Self::R>,
+        _shield: &'shield Shield<'_, Ebr<Self::M>>,
     ) -> Shared<'shield, Self::V, Self::T>;
 
     fn store<'shield>(&self, new: Shared<'_, Self::V, Self::T>, order: Ordering);
@@ -18,7 +19,7 @@ pub trait AtomicEbrExt {
         &self,
         new: Shared<'_, Self::V, Self::T>,
         order: Ordering,
-        _shield: &'shield Shield<'_, Self::R>,
+        _shield: &'shield Shield<'_, Ebr<Self::M>>,
     ) -> Shared<'shield, Self::V, Self::T>;
 
     fn compare_and_swap<'shield>(
@@ -26,23 +27,23 @@ pub trait AtomicEbrExt {
         current: Shared<'_, Self::V, Self::T>,
         new: Shared<'_, Self::V, Self::T>,
         order: Ordering,
-        _shield: &'shield Shield<'_, Self::R>,
+        _shield: &'shield Shield<'_, Ebr<Self::M>>,
     ) -> Shared<'shield, Self::V, Self::T>;
 }
 
-impl<R, V, T> AtomicEbrExt for Atomic<R, V, T>
+impl<M, V, T> AtomicEbrExt for Atomic<Ebr<M>, V, T>
 where
-    R: Reclaimer,
+    M: ReclaimableManager,
     T: Tag,
 {
-    type R = R;
+    type M = M;
     type V = V;
     type T = T;
 
     fn load<'shield>(
         &self,
         order: Ordering,
-        _shield: &'shield Shield<'_, Self::R>,
+        _shield: &'shield Shield<'_, Ebr<Self::M>>,
     ) -> Shared<'shield, Self::V, Self::T> {
         let data = self.data.load(order);
         Shared::from_raw(data)
@@ -57,7 +58,7 @@ where
         &self,
         new: Shared<'_, Self::V, Self::T>,
         order: Ordering,
-        _shield: &'shield Shield<'_, Self::R>,
+        _shield: &'shield Shield<'_, Ebr<Self::M>>,
     ) -> Shared<'shield, Self::V, Self::T> {
         let new_data = new.into_raw();
         let previous_data = self.data.swap(new_data, order);
@@ -69,7 +70,7 @@ where
         current: Shared<'_, Self::V, Self::T>,
         new: Shared<'_, Self::V, Self::T>,
         order: Ordering,
-        _shield: &'shield Shield<'_, Self::R>,
+        _shield: &'shield Shield<'_, Ebr<Self::M>>,
     ) -> Shared<'shield, Self::V, Self::T> {
         let current_data = current.into_raw();
         let new_data = new.into_raw();

52323dbdc4a5c28056419ff3fd42ea5b70a4b535
Acrimon <joel.wejdenstal@gmail.com>
2020-08-17 12:05:29 +0200

    2.0.0

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ mod tag;
 mod thread_local;
 
 pub use atomic::Atomic;
-pub use generic_array::typenum;
+pub use generic_array;
 pub use reclaim::{ReclaimableManager, Reclaimer};
 pub use shared::Shared;
 pub use shield::Shield;

0d2a5967556e1bf112251b58cc52ea5f167ac1f0
Acrimon <joel.wejdenstal@gmail.com>
2020-09-18 21:40:27 +0200

    chore: please clippy

--- src/barrier.rs
+++ src/barrier.rs
@@ -70,13 +70,13 @@ mod linux {
         #[allow(dead_code, non_camel_case_types)]
         enum membarrier_cmd {
             MEMBARRIER_CMD_QUERY = 0,
-            MEMBARRIER_CMD_GLOBAL = (1 << 0),
-            MEMBARRIER_CMD_GLOBAL_EXPEDITED = (1 << 1),
-            MEMBARRIER_CMD_REGISTER_GLOBAL_EXPEDITED = (1 << 2),
-            MEMBARRIER_CMD_PRIVATE_EXPEDITED = (1 << 3),
-            MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED = (1 << 4),
-            MEMBARRIER_CMD_PRIVATE_EXPEDITED_SYNC_CORE = (1 << 5),
-            MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED_SYNC_CORE = (1 << 6),
+            MEMBARRIER_CMD_GLOBAL = 1 << 0,
+            MEMBARRIER_CMD_GLOBAL_EXPEDITED = 1 << 1,
+            MEMBARRIER_CMD_REGISTER_GLOBAL_EXPEDITED = 1 << 2,
+            MEMBARRIER_CMD_PRIVATE_EXPEDITED = 1 << 3,
+            MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED = 1 << 4,
+            MEMBARRIER_CMD_PRIVATE_EXPEDITED_SYNC_CORE = 1 << 5,
+            MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED_SYNC_CORE = 1 << 6,
         }
 
         fn sys_membarrier(cmd: membarrier_cmd) -> libc::c_long {

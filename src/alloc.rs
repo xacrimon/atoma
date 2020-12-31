3cc97cf9de353c7e426f130d99b6606aca4028b0
Acrimon <joel.wejdenstal@gmail.com>
2020-12-31 01:50:07 +0100

    remove ub comment

--- src/alloc.rs
+++ src/alloc.rs
@@ -1,5 +1,4 @@
 // if you thought the `barrier` and `deferred` modules were cursed, hoo boy are you in for a surprise
-// this is probably UB as shit but it gets the job done, feel free to pr if you have a better solution, i don't
 
 use core::{
     fmt,
